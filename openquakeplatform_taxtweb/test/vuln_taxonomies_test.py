8d24287d752907a93ce2ab503d40a033f33c032e
Matteo Nastasi <nastasi@openquake.org>
2016-09-14 11:23:07 +0200

    omnicomprensive taxonomies test

--- openquakeplatform_taxtweb/test/vuln_taxonomies_test.py
+++ openquakeplatform_taxtweb/test/vuln_taxonomies_test.py
@@ -10,31 +10,45 @@ from openquakeplatform.test import pla
 class VulnTaxonomiesTest(unittest.TestCase):
     pass
 
+def tag_and_val_get(xpath, times):
+    resulte_tag = pla.xpath_finduniq(xpath, times=times)
+    resulte_val = resulte_tag.get_attribute("value")
+    return (resulte_tag, resulte_val)
+            
+
 def make_function(func_name, taxonomy):
     def generated(self):
         pla.get('/taxtweb/%s' % taxonomy)
-        resulte_tag = pla.xpath_finduniq("//input[@id='resultE']", times=20)
-
+        resulte_tag, resulte_val = tag_and_val_get("//input[@id='resultE']", 20)
         pla.waituntil_js(3, "gem_pageloaded == true;")
 
-        if resulte_tag.get_attribute("value") != taxonomy:
+        if resulte_val != taxonomy:
             print "\nA:[%s]\nB:[%s]\n" % (taxonomy, resulte_tag.get_attribute("value"))
-        self.assertEqual(resulte_tag.get_attribute("value"), taxonomy)
-            
-        # resulte_tag.click()   # Positions the cursor at the end of the string        
-        # resulte_tag.sendKeys(Keys.BACK_SPACE)
-        
-        #exp_filename = os.path.join(exp_path,
-        #                        "example_%d.%s" % (tab_id * 100 + example['exa_id'],
-        #                       example['sfx']))
-        #        with codecs.open(exp_filename, 'r', 'utf-8') as exp_file:
-        #    expected = exp_file.read()
+        self.assertEqual(resulte_val, taxonomy)
 
-        #ret = ret_tag.get_attribute("value")
-        #if ret is None:
-        #    ret = ret_tag.get_attribute('innerHTML')
-        #self.assertEqual(ret, expected)
+        resulte_tag.click()   # Positions the cursor at the end of the string        
+        if len(resulte_val) > 0 and resulte_val[-1] == '/':
+            resulte_tag.send_keys(Keys.BACK_SPACE)
+            resulte_tag, resulte_val = tag_and_val_get("//input[@id='resultE']", 20)
 
+        cur = resulte_val
+        while len(cur) > 0:            
+            last = cur[-1]
+            resulte_tag.send_keys(Keys.BACK_SPACE)
+            resulte_tag, resulte_val = tag_and_val_get("//input[@id='resultE']", 20)
+            resulte_bgcol = resulte_tag.value_of_css_property('background-color')
+            self.assertNotEqual(resulte_bgcol, "rgba(255, 223, 191, 1)")
+
+            if last is "/":
+                # check integrity
+                virtual_tag, virtual_val = tag_and_val_get("//input[@id='resultE_virt']", 20)
+
+                self.assertEqual(resulte_bgcol, "rgba(191, 255, 191, 1)")
+                self.assertEqual(resulte_val, virtual_val)
+
+            if resulte_val == "":
+                break
+                
     generated.__name__ = func_name
     return generated
 
@@ -47,7 +61,7 @@ def generator():
         for taxonomy in f:
             if taxonomy[0] == '#':
                 continue
-            if ct == 1000:
+            if ct == 30:
                 break
             taxonomy = taxonomy.strip()
             func_name = "%s_test" % taxonomy
